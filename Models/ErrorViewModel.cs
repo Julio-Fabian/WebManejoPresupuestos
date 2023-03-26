f62b10f4ba7f4be965d26b259e1ce4e9fa4c7e28
Julio Fabian <Fabian87_delarosa@outlook.com>
2023-03-26 13:01:55 -0600

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebManejoPresupuestos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
