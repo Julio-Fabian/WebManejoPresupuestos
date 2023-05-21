66aacce5996c79dcfb56d3b60cf4de2043156e0c
Julio Fabian <Fabian87_delarosa@outlook.com>
2023-05-21 17:00:16 -0600

    Mejoras y seguridad de modelo (prueba)

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace WebManejoPresupuestos.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
--- Models/TiposCuentas.cs
+++ Models/TiposCuentas.cs
@@ -3,18 +3,36 @@ using WebManejoPresupuestos.Validaciones;
 
 namespace WebManejoPresupuestos.Models
 {
-    public class TiposCuentas
+    public class TiposCuentas //: IValidatableObject
     {
         public int Id { get; set; }
 
         // validaciones del campo "Nombre"
         [Required (ErrorMessage = "El campo {0}, es obligatorio.")]
-        [Display(Name = "Nombre")]
-        [PrimeraLetraMayuscula]
+        [PrimeraLetraMayuscula] // se sobreentiende 'PrimeraLetraMayusculaAttribute'
+        [Display(Name = "Nombre cuenta")]
         public string Nombre { get; set; } //Nombre es obligatorio.
         
         public int UsuarioId { get; set; }
         
         public int Orden { get; set; }
+
+        /*
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0)
+            {
+                var letra = Nombre[0].ToString();
+
+                if (letra != letra.ToUpper()) 
+                {
+                    yield return new ValidationResult("La primer letra debe ser mayuscula",
+                        new[] { nameof(Nombre) } );
+                }
+            }
+        } 
+        
+        */
     }
 }
